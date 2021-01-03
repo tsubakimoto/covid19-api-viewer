135670c1a72800081c6b66710bd06535903ca840
tsubakimoto <tkmbmbr@gmail.com>
2021-01-03 18:10:39 +0900

    ナビゲーションバーの設置

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,11 @@
 <template>
   <b-container>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand href="/">covid19-api-viewer</b-navbar-brand>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item href="/login">ログイン</b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
     <b-jumbotron header="covid19-api-viewer">
       <p>Nuxt.jsの練習アプリ</p>
       <nuxt-link to="apis" class="btn btn-primary btn-lg">
@@ -9,7 +15,7 @@
         href="https://github.com/tsubakimoto/covid19-api-viewer"
         target="_blank"
         rel="noopener noreferrer"
-        class="btn btn-info btn-lg"
+        class="btn btn-dark btn-lg"
       >
         GitHub
       </a>
